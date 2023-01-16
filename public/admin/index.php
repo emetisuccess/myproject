f7e876cb6d10081ff9831836904ec67f7b3996e5
Emeti <65733999+emetisuccess@users.noreply.github.com>
2023-01-16 06:45:41 +0100

    Add files via upload

--- public/admin/index.php
+++ public/admin/index.php
@@ -10,28 +10,40 @@
                 <div class="col-md-12">
                     <?php
                     if (
-                        $_SERVER['REQUEST_URI'] == "/myproject/public/admin/" or
-                        $_SERVER['REQUEST_URI'] == "/myproject/public/admin/index.php"
+                        $_SERVER['REQUEST_URI'] == "/myproject-development/public/admin/" or
+                        $_SERVER['REQUEST_URI'] == "/myproject-development/public/admin/index.php"
                     ) {
                         include(TEMPLATE_BACK . DS . "admin_content.php");
                     }
-                    if (isset($_GET['add_user'])) {
-                        echo "add user";
-                    }
                     if (isset($_GET['manage_user'])) {
-                        echo "manage users";
+                        require(TEMPLATE_BACK . DS . "manage_user.php");
+                    }
+                    if (isset($_GET['add_brand'])) {
+                        require(TEMPLATE_BACK . DS . "add_brand.php");
+                    }
+                    if (isset($_GET['manage_brand'])) {
+                        require(TEMPLATE_BACK . DS . "manage_brand.php");
                     }
                     if (isset($_GET['add_category'])) {
-                        echo "categories";
+                        require(TEMPLATE_BACK . DS . "add_category.php");
                     }
                     if (isset($_GET['manage_category'])) {
-                        echo "manage categories";
+                        require(TEMPLATE_BACK . DS . "manage_category.php");
+                    }
+                    if (isset($_GET['manage_booking'])) {
+                        require(TEMPLATE_BACK . DS . "manage_booking.php");
                     }
                     if (isset($_GET['add_product'])) {
                         require(TEMPLATE_BACK . DS . "add_product.php");
                     }
+                    if (isset($_GET['manage_driver'])) {
+                        require(TEMPLATE_BACK . DS . "manage_drivers.php");
+                    }
                     if (isset($_GET['manage_product'])) {
                         require(TEMPLATE_BACK . DS . "manage_product.php");
+                    }
+                    if (isset($_GET['manage_towing'])) {
+                        require(TEMPLATE_BACK . DS . "manage_towing.php");
                     }
                     if (isset($_GET['update_sparepart_product'])) {
                         require(TEMPLATE_BACK . DS . "update_product.php");
@@ -48,21 +60,22 @@
                     if (isset($_GET['manage_accessory'])) {
                         require(TEMPLATE_BACK . DS . "manage_accessory.php");
                     }
-                    if (isset($_GET['add_comment'])) {
-                        echo "add_comment";
-                    }
-                    if (isset($_GET['manage_comments'])) {
-                        echo "manage_comment";
-                    }
-                    if (isset($_GET['add_mechanic'])) {
-                        echo "add_mechanic";
-                    }
                     if (isset($_GET['manage_mechanic'])) {
-                        echo "manage_mechanic";
+                        require(TEMPLATE_BACK . DS . "manage_mechanic.php");
                     }
                     if (isset($_GET['edit_vehicle'])) {
                         require(TEMPLATE_BACK . DS . "update_vehicle.php");
                     }
+                    if (isset($_GET['edit_booking'])) {
+                        require(TEMPLATE_BACK . DS . "edit_booking.php");
+                    }
+                    if (isset($_GET['edit_cat'])) {
+                        require(TEMPLATE_BACK . DS . "edit_category.php");
+                    }
+                    if (isset($_GET['edit_brand'])) {
+                        require(TEMPLATE_BACK . DS . "edit_brand.php");
+                    }
+
                     ?>
                 </div>
             </div>
